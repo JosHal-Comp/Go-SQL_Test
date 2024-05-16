888bdf539bb37c1e7f93be99f70e7604d39cf916
JosHal-Comp <josephhalim1001@gmail.com>
2024-05-16 14:29:03 +0700

    API Endpoint Added

--- TaskSorter.go
+++ TaskSorter.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -24,15 +28,21 @@ type TaskList struct {
 	next  *TaskList
 }
 
+type GetList struct {
+	Code  int    `json:"code"`
+	Task  string `json:"task"`
+	Exist bool   `json:"exist"`
+}
+
 func main() {
 	List := new(TaskList)
 	List = List.read_database()
 
-	fmt.Printf("Commands for task list:\n1-Insert Task\n2-Delete Task\n3-Exit Program\n")
+	fmt.Printf("Commands for task list:\n1-Insert Task\n2-Delete Task\n3-API\n4-Exit Program\n")
 	List.list()
 	command := 0
 
-	for command != 3 {
+	for command != 3 && command != 4 {
 		fmt.Printf("Enter command: ")
 		fmt.Scanf("%d\n", &command)
 
@@ -42,12 +52,16 @@ func main() {
 		} else if command == 2 {
 			List = List.delete()
 			List.list()
-		} else if command == 3 {
+		} else if command == 3 || command == 4 {
 			break
 		} else {
-			fmt.Printf("\nCommands for task list:\n1-Insert Task\n2-Delete Task\n3-Exit Program\n")
+			fmt.Printf("\nCommands for task list:\n1-Insert Task\n2-Delete Task\n3-API\n4-Exit Program\n")
 			List.list()
 		}
+	}
+
+	if command == 3 {
+		handle_requests()
 	}
 
 	List.close_database()
@@ -133,7 +147,7 @@ func (curr *TaskList) read_database() *TaskList {
 
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM Tasks")
+	rows, err := db.Query("SELECT * FROM Tasks ORDER BY code")
 
 	if err != nil {
 		panic(err)
@@ -184,6 +198,7 @@ func (curr *TaskList) close_database() {
 
 	for curr_pos != nil {
 		err = db.QueryRow("SELECT COUNT(*) FROM Tasks WHERE code = $1", curr_pos.code).Scan(&count)
+
 		if err != nil {
 			panic(err)
 		}
@@ -194,12 +209,165 @@ func (curr *TaskList) close_database() {
 				panic(err)
 			}
 		} else if count == 0 {
-			_, err := db.Exec("INSERT INTO Tasks (code, task, exist) VALUES ($1, $2, $3)", curr_pos.code, curr_pos.task, curr_pos.exist)
+			_, err := db.Exec("INSERT INTO Tasks (task, exist) VALUES ($1, $2)", curr_pos.task, curr_pos.exist)
 			if err != nil {
 				panic(err)
 			}
 		}
 
 		curr_pos = curr_pos.next
+	}
+}
+
+func get_tasks(w http.ResponseWriter, r *http.Request) {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM Tasks ORDER BY code")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	var tasks []GetList
+
+	for rows.Next() {
+		var task GetList
+		err := rows.Scan(&task.Code, &task.Task, &task.Exist)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func create_task(w http.ResponseWriter, r *http.Request) {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	var insert_task GetList
+	params := mux.Vars(r)
+	insert_task.Task = params["task"]
+
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	count := 0
+
+	err = db.QueryRow("SELECT COUNT(*) FROM Tasks WHERE task = $1", insert_task.Task).Scan(&count)
+
+	if err != nil {
+		panic(err)
+	}
+
+	deleted := 0
+
+	if count == 1 {
+		existence := true
+		err = db.QueryRow("SELECT exist FROM Tasks WHERE task = $1", insert_task.Task).Scan(&existence)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if !existence {
+			deleted = 1
+		}
+	} else {
+		_, err = db.Exec("INSERT INTO Tasks (task, exist) VALUES ($1, $2)", insert_task.Task, true)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if deleted == 1 {
+		_, err = db.Exec("UPDATE Tasks SET exist = $1 WHERE task = $2", true, insert_task.Task)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func delete_task(w http.ResponseWriter, r *http.Request) {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	count := 0
+
+	err = db.QueryRow("SELECT COUNT(*) FROM Tasks WHERE code = $1", id).Scan(&count)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if count == 1 {
+		_, err = db.Exec("UPDATE Tasks SET exist = $1 WHERE code = $2", false, id)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func handle_requests() {
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", get_tasks).Methods("GET")
+	router.HandleFunc("/tasks/{task}", create_task).Methods("POST")
+	router.HandleFunc("/tasks/{id}", delete_task).Methods("DELETE")
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
 	}
 }
